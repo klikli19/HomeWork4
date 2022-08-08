cb44ae1968d14ef1cae4399fc41864e9c22c59f5
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-08-08 21:35:55 +0300

    сделала 1 задание

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main (String [] args) {
         System.out.println("Задание 1");
+        int a = 0;
+        while (a < 10){
+        a++;
+        System.out.print(a + " ");
+        }
+        System.out.println();
+        for (int b = 10; b > 0; b-- ) {
+            System.out.print(b + " ");
+        }
+
+
+
 
     }
 }
