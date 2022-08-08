020ca68c0b401adabc18bb69eff8c8b0a9e1d2b6
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-08-08 21:46:11 +0300

    сделала 2 задание

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,12 @@ public class Main {
         for (int b = 10; b > 0; b-- ) {
             System.out.print(b + " ");
         }
+        System.out.println();
+
+        System.out.println("Задание 2");
+        for (int friday = 7; friday < 31;  friday = friday + 7) {
+            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет");
+        }
 
 
 
