c1bd2113c1954bbdcd1f6ed1ba13d1f89761f6ab
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-08-08 23:52:26 +0300

    сделала 3 задание

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,34 @@
 public class Main {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         System.out.println("Задание 1");
         int a = 0;
-        while (a < 10){
-        a++;
-        System.out.print(a + " ");
+        while (a < 10) {
+            a++;
+            System.out.print(a + " ");
         }
         System.out.println();
-        for (int b = 10; b > 0; b-- ) {
+        for (int b = 10; b > 0; b--) {
             System.out.print(b + " ");
         }
         System.out.println();
 
         System.out.println("Задание 2");
-        for (int friday = 7; friday < 31;  friday = friday + 7) {
+        for (int friday = 7; friday < 31; friday = friday + 7) {
             System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет");
         }
+        System.out.println();
 
+        System.out.println("Задание 3");
+        int yearNow = 2022;
+        int yearStop = yearNow + 100;
 
-
-
+        for (int start = 79; start <= yearStop; start = start + 79) {
+        }
+        for (int comet = yearNow - 200; comet <= yearStop; comet++ ) {
+            if ( comet % 79 == 0)
+            System.out.println(comet);
+        }
     }
 }
+
+
